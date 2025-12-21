dce4ed1dcebc7c696d9e8cb1e27aa9f02be15d19
primanafisman <prinafis@gmail.com>
2025-12-21 21:42:00 +0700

    Update file report and simulation

--- Task 2/param.m
+++ Task 2/param.m
@@ -1,11 +1,13 @@
-Vd_total = 400;      
-V_source = Vd_total/2;
-R_load = 50;
+Vd_total 	= 400;      
+V_source 	= Vd_total/2;
+R_load 		= 50;
+L_filter 	= 4.5e-3;
+C_filter 	= 10e-6;
 
-ma = 1;
-V_carrier = 1;
+ma 			= 0.6;
+V_carrier 	= 1;
 V_ref = ma * V_carrier;
 
-f_sw = 5000;
-f_fund = 50;
+f_sw 		= 5000;
+f_fund 		= 50;
 
